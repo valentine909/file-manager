f70a3550bd3f019e6ca4416d6e86624439977ede
Valentine <valentine.nazarov@gmail.com>
2022-06-09 19:34:44 +0300

    refactor: export string variables

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 import { homedir } from 'os';
 import { createInterface } from 'readline';
 import { parseArgs } from './parse.js';
-import { settings } from './settings.js';
+import { messages, settings } from './settings.js';
 
 const mainLoop = async () => {
-  console.log(`Welcome to the File Manager, ${settings.username}!`);
-  console.log(`You are currently in ${settings.currentPath}`);
+  console.log(messages.hello + settings.username + '!');
+  console.log(messages.currentPath + settings.currentPath);
   const readlineInterface = createInterface({
     input: process.stdin,
     output: process.stdout,
   });
   readlineInterface.on('line', async (line) => {
     await parseArgs(line.split(' '));
-    console.log(`\nYou are currently in ${settings.currentPath}`);
+    console.log(messages.currentPath + settings.currentPath);
   });
   process.on('exit', () => {
     readlineInterface.close();
-    console.log(`Thank you for using File Manager, ${settings.username}!`);
+    console.log(messages.goodbye + settings.username + '!');
   });
 };
 
 const validateArgs = () => {
   if (process.argv.length <= 2) {
-    console.log('Insufficient arguments');
+    console.log(messages.insufficientArgs);
     process.exit();
   }
 };
 
 const validateUsername = (flag) => {
   if (flag !== '--username' || !settings.username) {
-    console.log('Incorrect username');
+    console.log(messages.incorrectUsername);
     process.exit();
   }
 };
