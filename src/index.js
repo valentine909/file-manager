fe181d59645eb75e85f4438fda5e77369504250d
Valentine <valentine.nazarov@gmail.com>
2022-06-09 16:13:56 +0300

    refactor: mainLoop and init

--- src/index.js
+++ src/index.js
@@ -1,26 +1,49 @@
-import os from "os";
-import { createInterface } from "readline";
+import os from 'os';
+import { createInterface } from 'readline';
 
-const validateStartFlag = (flag) => {
-  return flag === "--username";
+export const settings = {
+  currentPath: '',
+  username: '',
 };
 
-let flag;
-let username;
-[flag, username] = process.argv.slice(2)[0].split("=");
-console.log(`Welcome to the File Manager, ${username}!`);
+async function mainLoop() {
+  console.log(`Welcome to the File Manager, ${settings.username}!`);
+  console.log(`You are currently in ${settings.currentPath}`);
+  const readlineInterface = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  readlineInterface.on('line', (line) => {
+    console.log(line);
+    console.log(`You are currently in ${settings.currentPath}`);
+  });
+  process.on('exit', () => {
+    readlineInterface.close();
+    console.log(`Thank you for using File Manager, ${settings.username}!`);
+  });
+}
 
-console.log(validateStartFlag(flag));
-let currentPath = os.homedir();
-console.log(`You are currently in ${currentPath}`);
-const readlineInterface = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-readlineInterface.on("line", (line) => {
-  console.log(line);
-});
-process.on("SIGINT", () => {
-  readlineInterface.close();
-  console.log(`Thank you for using File Manager, ${username}!`);
-});
+const validateArgs = () => {
+  if (process.argv.length <= 2) {
+    console.log('Insufficient arguments');
+    process.exit();
+  }
+};
+
+const validateUsername = (flag) => {
+  if (flag !== '--username' || !settings.username) {
+    console.log('Incorrect username');
+    process.exit();
+  }
+};
+
+function init() {
+  let flag;
+  settings.currentPath = os.homedir();
+  validateArgs();
+  [flag, settings.username] = process.argv.slice(2)[0].split('=');
+  validateUsername(flag);
+  mainLoop().then();
+}
+
+init();
